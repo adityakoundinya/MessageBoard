171777f292913148f29f45f8a22bb2950907b6d9
adityakoundinya <aditya.koundinya@gmail.com>
2016-01-22 14:18:51 -0600

    Added AngularJS

---
+++ MessageBoard/js/home-index.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module("messageBoard", []);
+app.controller("homeIndexController", function ($scope, $http) {
+    $scope.data = [];
+    $scope.isBusy = true;
+
+    $http.get("/api/v1/topics?includeReplies=true")
+    .then(function (result) {
+        angular.copy(result.data, $scope.data);
+    },
+    function () {
+        alert("could not load topics");
+    })
+    .then(function () {
+        $scope.isBusy = false;
+    });
+});
